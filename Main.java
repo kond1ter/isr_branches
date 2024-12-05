30c1dcab365d9c04caf57d2ea7a741153873a095
konditer <konditer1324@gmail.com>
2024-12-05 03:05:32 +0300

    update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это также добавлено в ветке main");
+        System.out.print("Это еще раз добавлено в ветке main");
     }
 }
